88d696f0dea9b4a540e6a5259f7e1e7bf5206357
electblake <electblake@gmail.com>
2011-07-22 17:23:09 -0400

    init commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'druthemer.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Locus New Media Examples</title>
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link href="<?php print base_path().path_to_theme(); ?>index.css" rel="stylesheet" type="text/css" media="screen,projection,print" />
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script>
+</head>
+<body>
+	<h1>Locus New Media - Prototypes</h1>
+	<ul>
+		<li><a href="<?php print base_path(); ?>parallax">Parallax Prototype</a></li>
+	</ul>
+</body>
+</html>
---
+++ parallax/index.php
@@ -0,0 +1,40 @@
+<?php include 'druthemer.php'; ?>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset=utf-8>
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+
+	<title>Parallax for Locus New Media</title>
+
+	<script type="text/javascript" src="media/j/lib/modernizr-1.7.min.js" charset="utf-8"></script>
+
+	<link href="media/c/lib/reset.css" rel="stylesheet" type="text/css" media="screen,projection,print" />
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script>
+	
+	<!--[if lte IE 6]>
+	<link href="media/c/ie6.css" rel="stylesheet" type="text/css" media="screen,projection" />
+	<![endif]-->
+	<!--[if lte IE 7]>
+	<link href="media/c/ie7.css" rel="stylesheet" type="text/css" media="screen,projection" />
+	<![endif]-->
+	
+	<meta name="viewport" content="width=640, maximum-scale=1.0">
+</head>
+<body class="">
+<section id="parts">
+	<img src="<?php print base_path().path_to_theme(); ?>/_images/monster-eyesgreen.png" id="monster_green" />
+	<img src="<?php print base_path().path_to_theme(); ?>/_images/monster-orangeninka.png" id="monster_orange" />
+	<img src="<?php print base_path().path_to_theme(); ?>/_images/monster-pink.png" id="monster_pink" />
+	<img src="<?php print base_path().path_to_theme(); ?>/_images/monster-purple.png" id="monster_purple" />
+	<img src="<?php print base_path().path_to_theme(); ?>/_images/monster-questiongrey.png" id="monster_grey" />
+	<img src="<?php print base_path().path_to_theme(); ?>/_images/monster-starteal" id="monster_teal" />
+	<img src="<?php print base_path().path_to_theme(); ?>/_images/monster-tabletblack.png" id="monster_black" />
+	<img src="<?php print base_path().path_to_theme(); ?>/_images/monster-techblue.png" id="monster_blue" />
+	<img src="<?php print base_path().path_to_theme(); ?>/_images/monster-tvbrown.png" id="monster_brown" />
+</section>
+<script src="<?php print base_path().path_to_theme(); ?>/_js/parallax.js" type="text/javascript"></script>
+</body>
+</html>
